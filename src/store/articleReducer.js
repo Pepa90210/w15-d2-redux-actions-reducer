5753ddb8a04c9a6932dcf03a29f21ebc1cfe57a3
Daniel Hoffmann <103540391+Pepa90210@users.noreply.github.com>
2022-08-16 12:44:17 -0500

    done

---
+++ src/store/articleReducer.js
@@ -0,0 +1,26 @@
+import articles from "../data/data.json";
+
+// type
+const LOAD_ARTICLES = "article/loadArticles";
+
+// actions
+export const loadArticles = () => {
+  return {
+    type: LOAD_ARTICLES,
+    articles
+  };
+};
+
+// reducer
+const initialState = { entries: [], isLoading: true };
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_ARTICLES:
+      return { ...state, entries: [...action.articles] };
+    default:
+      return state;
+  }
+};
+
+export default articleReducer;
